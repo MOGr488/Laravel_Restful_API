93e3bcc7df29d53d9c5aa2b4fb5c28ccc802f5ca
MOGr488 <mogr488@gmail.com>
2023-04-04 21:39:14 +0400

    Setting up API routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Lesson;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,35 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Get all lessons
+Route::get('lessons', function(){
+    return Lesson::all();
+});
+
+// Get one lesson
+Route::get('lessons/{id}', function($id){
+    return Lesson::find($id);
+});
+
+// Create a lesson
+Route::post('lessons', function(Request $request){
+    return Lesson::create($request->all());
+});
+
+// Update a lesson
+Route::put('lessons/{id}', function(Request $request, $id){
+    $lesson = Lesson::findOrfail($id);
+    $lesson->update($request->all());
+
+    return $lesson;
+});
+
+
+// Delete a lesson 
+Route::delete('lessons/{id}', function($id){
+    return Lesson::find($id)->delete();
+    return 204;
+});
+
+
